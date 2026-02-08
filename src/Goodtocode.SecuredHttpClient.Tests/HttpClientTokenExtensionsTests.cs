82d1a4302cf01d4356a46c135ca50b95dd85a907
Robert Good <robert.good@goodtocode.com>
2026-02-07 19:09:28 -0800

    tests

--- src/Goodtocode.SecuredHttpClient.Tests/HttpClientTokenExtensionsTests.cs
+++ src/Goodtocode.SecuredHttpClient.Tests/HttpClientTokenExtensionsTests.cs
@@ -20,4 +20,34 @@ public class HttpClientTokenExtensionsTests
         Assert.AreEqual("Bearer", client.DefaultRequestHeaders.Authorization?.Scheme);
         Assert.AreEqual(token, client.DefaultRequestHeaders.Authorization?.Parameter);
     }
+
+    [TestMethod]
+    public void AddBearerTokenOverwritesExistingAuthorizationHeader()
+    {
+        // Arrange
+        var client = new System.Net.Http.HttpClient();
+        client.DefaultRequestHeaders.Authorization =
+            new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", "old");
+
+        // Act
+        client.AddBearerToken("new-token");
+
+        // Assert
+        Assert.AreEqual("Bearer", client.DefaultRequestHeaders.Authorization?.Scheme);
+        Assert.AreEqual("new-token", client.DefaultRequestHeaders.Authorization?.Parameter);
+    }
+
+    [TestMethod]
+    public void AddBearerTokenAllowsEmptyToken()
+    {
+        // Arrange
+        var client = new System.Net.Http.HttpClient();
+
+        // Act
+        client.AddBearerToken(string.Empty);
+
+        // Assert
+        Assert.AreEqual("Bearer", client.DefaultRequestHeaders.Authorization?.Scheme);
+        Assert.AreEqual(string.Empty, client.DefaultRequestHeaders.Authorization?.Parameter);
+    }
 }
